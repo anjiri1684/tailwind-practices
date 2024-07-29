7874fc93842e232b5b31ce27d28eda87b78b7efd
vincent anjiri <vincentanjiri12@gmail.com>
2024-07-29 16:24:57 +0300

    initial commit

--- shortly/js/script.js
+++ shortly/js/script.js
@@ -1,46 +1,48 @@
-const btn = document.getElementById('menu-btn')
-const menu = document.getElementById('menu')
+const btn = document.getElementById("menu-btn");
+const menu = document.getElementById("menu-btn");
 
-const input = document.getElementById('link-input')
-const linkForm = document.getElementById('link-form')
-const errMsg = document.getElementById('err-msg')
+const input = document.getElementById("link-input");
+const linkForm = document.getElementById("link-form");
+const errMsg = document.getElementById("err-msg");
 
-btn.addEventListener('click', navToggle)
-linkForm.addEventListener('submit', formSubmit)
+linkForm.addEventListener("submit", formSubmit);
 
 // Toggle Mobile Menu
+
 function navToggle() {
-  btn.classList.toggle('open')
-  menu.classList.toggle('flex')
-  menu.classList.toggle('hidden')
+  btn.classList.toggle("open");
+  menu.classList.toggle("flex");
+  menu.classList.toggle("hidden");
 }
+
+btn.addEventListener("click", navToggle);
 
 // Validate a URL
 function validURL(str) {
   var pattern = new RegExp(
-    '^(https?:\\/\\/)?' + // protocol
-      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
-      '((\\d{1,3}\\.){3}\\d{1,3}))' +
-      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
-      '(\\?[;&a-z\\d%_.~+=-]*)?' +
-      '(\\#[-a-z\\d_]*)?$',
-    'i'
-  )
-  return !!pattern.test(str)
+    "^(https?:\\/\\/)?" + // protocol
+      "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" +
+      "((\\d{1,3}\\.){3}\\d{1,3}))" +
+      "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" +
+      "(\\?[;&a-z\\d%_.~+=-]*)?" +
+      "(\\#[-a-z\\d_]*)?$",
+    "i"
+  );
+  return !!pattern.test(str);
 }
 
 function formSubmit(e) {
-  e.preventDefault()
+  e.preventDefault();
 
-  if (input.value === '') {
-    errMsg.innerHTML = 'Please enter something'
-    input.classList.add('border-red')
+  if (input.value === "") {
+    errMsg.innerHTML = "Please enter something";
+    input.classList.add("border-red");
   } else if (!validURL(input.value)) {
-    errMsg.innerHTML = 'Please enter a valid URL'
-    input.classList.add('border-red')
+    errMsg.innerHTML = "Please enter valid url";
+    input.classList.add("border-red");
   } else {
-    errMsg.innerHTML = ''
-    input.classList.remove('border-red')
-    alert('Success')
+    errMsg.innerHTML = "";
+    input.classList.remove("border-red");
+    alert("Success");
   }
 }
