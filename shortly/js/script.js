d21d3eb30be9c2e569fa503aa2ed4cbd912b45b2
vincent anjiri <vincentanjiri12@gmail.com>
2024-07-30 09:51:55 +0300

    initial commit

--- shortly/js/script.js
+++ shortly/js/script.js
@@ -1,21 +1,19 @@
 const btn = document.getElementById("menu-btn");
-const menu = document.getElementById("menu-btn");
+const menu = document.getElementById("menu");
 
 const input = document.getElementById("link-input");
 const linkForm = document.getElementById("link-form");
 const errMsg = document.getElementById("err-msg");
 
+btn.addEventListener("click", navToggle);
 linkForm.addEventListener("submit", formSubmit);
 
 // Toggle Mobile Menu
-
 function navToggle() {
   btn.classList.toggle("open");
   menu.classList.toggle("flex");
   menu.classList.toggle("hidden");
 }
-
-btn.addEventListener("click", navToggle);
 
 // Validate a URL
 function validURL(str) {
@@ -38,7 +36,7 @@ function formSubmit(e) {
     errMsg.innerHTML = "Please enter something";
     input.classList.add("border-red");
   } else if (!validURL(input.value)) {
-    errMsg.innerHTML = "Please enter valid url";
+    errMsg.innerHTML = "Please enter a valid URL";
     input.classList.add("border-red");
   } else {
     errMsg.innerHTML = "";
